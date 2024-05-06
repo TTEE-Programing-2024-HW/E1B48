eb2924b5b8d540102c2f5326d5fb0bc0bd518b65
EFisTB <u11202248@o365.ttu.edu.tw>
2024-05-06 21:24:18 +0800

    製作個人風格畫面，並利用迴圈做出20行

--- hw2.cpp
+++ hw2.cpp
@@ -2,5 +2,13 @@
 #include<stdlib.h>
 int main(void)
 {
+    printf("版權所有，翻印必究\n");
+    for(int i=1;i<20;i++) /*個人風格畫面*/
+    {
+        printf("版權所有，翻印必究\n");  
+    }
+    printf("按ENTER進入下一步");
+    getchar();  
+    system("cls");
     return 0; 
 }
