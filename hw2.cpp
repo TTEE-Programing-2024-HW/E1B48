91e4c7f306448b655dd4c4c967b2fd5036f8cb63
EFisTB <u11202248@o365.ttu.edu.tw>
2024-05-06 21:30:22 +0800

    定義數值，並用於輸入密碼和計算輸入的錯誤次數。

--- hw2.cpp
+++ hw2.cpp
@@ -10,5 +10,26 @@ int main(void)
     printf("按ENTER進入下一步");
     getchar();  
     system("cls");
+    int password, times = 0; /*輸入密碼*/ 
+    while (times < 3) /*當錯誤次數小於3時，所執行的結果*/ 
+    {
+        printf("密碼:");
+        scanf("%d", &password);
+        if (password != 2024) 
+        {
+            printf("密碼錯誤 再試一次\n");
+            times++;
+        } 
+
+
+
+
+
+
+
+
+
+
+
     return 0; 
 }
